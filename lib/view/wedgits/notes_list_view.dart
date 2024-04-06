982509104c15284e936f4f9907748d548c01581b
haider-x <haider.ahmed1846@gmail.com>
2024-04-07 01:46:45 +0300

    x

---
+++ lib/view/wedgits/notes_list_view.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:notesapp_git/view/wedgits/custom_note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder:(context,index){
+      return const Padding(
+        padding: EdgeInsets.symmetric(vertical: 8),
+        child: NoteItem(),
+      );
+    });
+  }
+}
--- lib/view/wedgits/notes_view_body.dart
+++ lib/view/wedgits/notes_view_body.dart
@@ -1,18 +1,23 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:notesapp_git/view/wedgits/custom_app_bar.dart';
+import 'package:notesapp_git/view/wedgits/custom_note_item.dart';
+import 'package:notesapp_git/view/wedgits/notes_list_view.dart';
+
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return  const Padding(
       padding: EdgeInsets.symmetric(horizontal: 24),
       child:Column(
         children: [
           SizedBox(height: 50,),
           CustomAppBar(),
-          NoteItem(),
+          Expanded(child: NotesListView()),
 
         ],
       ) ,); 
