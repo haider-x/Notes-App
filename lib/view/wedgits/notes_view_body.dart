8cc950a7093f92c8a3d9e1d7b3c991edb216f22f
haider-x <haider.ahmed1846@gmail.com>
2024-04-07 01:46:35 +0300

    xx

--- lib/view/wedgits/notes_view_body.dart
+++ lib/view/wedgits/notes_view_body.dart
@@ -18,37 +18,3 @@ class NotesViewBody extends StatelessWidget {
       ) ,); 
   }
 }
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCCBB),
-        borderRadius: BorderRadius.circular(16)
-      ), 
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile( 
-            title: const Text('Flutter Tips',style: TextStyle(
-              color: Colors.black,fontSize: 26,
-            ),),
-            subtitle:  Text('Build your career with Tharwat Samy',style: TextStyle(
-              color: Colors.black.withOpacity(.4),
-              ,fontSize: 20,
-            ),),
-            trailing: IconButton(
-              onPressed: (){},
-               icon: const Icon(Icons.delete,color: Colors.black,)),
-          ),
-          const Text('May21 , 2022',style: TextStyle(
-              color: Colors.black,
-            ),)
-
-        ],
-      ) ,
-    );
-  }
-}
